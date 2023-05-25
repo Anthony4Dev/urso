27e87ef2af439b95e5df3996fb0e3985fb1896c7
Anthony4Dev <anthony.islan2005@gmail.com>
2023-05-25 12:15:38 -0300

    post

--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,23 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { urso } from "@prisma/client";
 import cors from '@fastify/cors';
 import { request } from 'http';
+
+const prisma = new PrismaClient();
+const app = Fastify();
+app.register(cors, {
+    origin: "*",
+});
+
+app.post('/create', async (request: FastifyRequest, reply: FastifyReply) => {
+    const { id, name, age, description, gender} = request.body as urso;
+    const urso = await prisma.urso.create({
+        data: {
+            id,
+            name,
+            age,
+            description,
+            gender,
+        },
+    });
+    reply.send('Urso created')
+});
